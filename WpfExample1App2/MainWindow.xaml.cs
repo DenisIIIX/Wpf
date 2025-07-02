50eb6b9fa12735eb0cdc0c7d1d231ee3adb90350
денис волков <vdv.duf@mail.ru>
2025-07-02 23:12:03 +0300

    fixДобавьте файлы проекта.

--- WpfExample1App2/MainWindow.xaml.cs
+++ WpfExample1App2/MainWindow.xaml.cs
@@ -8,15 +8,15 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
-namespace WpfApp1
+namespace WpfExample1App2
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int i = 2;
         public MainWindow()
         {
             InitializeComponent();
@@ -24,8 +24,23 @@ namespace WpfApp1
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            int number = Convert.ToInt32(NumberTextBlock.Text) + 10;
-            NumberTextBlock.Text = number.ToString();
+            i = (i + 1)%3;
+            Red.Fill = Brushes.Gray;
+            Yellow.Fill = Brushes.Gray;
+            Green.Fill = Brushes.Gray;
+
+            switch (i)
+            {
+                case 0:
+                    Red.Fill = Brushes.Red;
+                    break;
+                case 1:
+                    Yellow.Fill = Brushes.Yellow;
+                    break;
+                case 2:
+                    Green.Fill = Brushes.Green;
+                    break;
+            }
         }
     }
 }
